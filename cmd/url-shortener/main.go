fc25e8f6d0a34349cd44fbd8e2fc70cc6ca39e38
Lyudmila <lyudmila.er@bk.ru>
2025-09-13 17:29:42 +0300

    add sqlite

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,21 +1,56 @@
 // TODO: init config: cleanenv
 package main
 
+// go run .\cmd\url-shortener
+
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"url-shortener/internal/config1"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage/sqlite"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config1.MustLoad()
+	log := setupLogger(cfg.Env)
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
-	fmt.Printf("Env=%s\nStorage=%s\nHTTP=%+v\n", cfg.Env, cfg.StoragePath, cfg.HTTPServer)
+	// fmt.Printf("Env=%s\nStorage=%s\nHTTP=%+v\n", cfg.Env, cfg.StoragePath, cfg.HTTPServer)
+
+	// TODO: init storage: sqlite
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
 }
 
-// TODO: init logger: slog
+// конфигурация логгера
+// slog - обёртка для логгера
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
 
-// TODO: init storage: sqlite
+	return log
+}
 
 // TODO: init router: chi, "chi render"
 
